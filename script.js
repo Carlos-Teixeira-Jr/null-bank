bc7c52769956b76014a43cd05a8297526b1ad0b6
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2025-01-07 13:40:04 -0300

    feat: second section of home pae finished

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const userAvatares = [];
 const featuresCards = [
   {
-    icon: `/assets/images/cash.svg`,
+    icon: `assets/images/gift-icon.svg`,
     title: "Recompensas",
     text: "Acumule pontos em cada compra e troque por produtos, serviços, milhas aéreas e muito mais.",
   },
   {
-    icon: `/assets/images/credit-card.svg`,
+    icon: `assets/images/percent-icon.svg`,
     title: "Descontos",
     text: "Taxas de juros e tarifas de serviços competitivas, oferecendo melhores condições.",
   },
   {
-    icon: `/assets/images/credit-card.svg`,
+    icon: `assets/images/cash-icon.svg`,
     title: "Cashback",
     text: "Ganhe uma porcentagem do valor gasto de volta em todas as compras.",
   },
   {
-    icon: `/assets/images/credit-card.svg`,
+    icon: `assets/images/credit-card-icon.svg`,
     title: "Cartão Internacional",
     text: "Geração de cartões virtuais para compras online, garantindo maior segurança e controle.",
+  },
+  {
+    icon: `assets/images/credit-card-icon.svg`,
+    title: "Carteiras Digitais",
+    text: "Aceito em milhões de estabelecimentos ao redor do mundo, facilitando compras e viagens internacionais.",
   },
 ];
 
@@ -27,7 +32,7 @@ for (let i = 1; i <= 5; i++) {
   userAvatares.push(imgPath);
 }
 
-userAvatares.push(`assets/images/user-round.svg`)
+userAvatares.push(`assets/images/user-round.svg`);
 
 const avatarContainer = document.getElementById("avatar-container");
 
@@ -37,9 +42,9 @@ userAvatares.map((path, idx) => {
   imgElement.classList.add("avatar");
   imgElement.style.left = `${(idx + 3.5) * 30}px`;
   avatarContainer.appendChild(imgElement);
-  
+
   if (idx === userAvatares.length - 1) {
-    imgElement.style.backgroundColor = '#EEE5FF'
+    imgElement.style.backgroundColor = "#EEE5FF";
   }
 });
 
@@ -57,20 +62,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   featuresCards.map((card, idx) => {
     const cardElement = document.createElement("div");
-    cardElement.classList.add("benefit-card");
-    cardElement.innerHTML = `
-      <div class="card-icon-container">
-        <img src="${card.icon}" alt="${card.title}">
-        <h3>${card.title}</h3>
-      </div>
-      <p>${card.text}</p>
-    `;
 
-    if (idx % 2 === 0) {
-      cardElement.style.width = "382px";
-    } else {
-      cardElement.style.width = "330px";
+    cardElement.innerHTML = idx === featuresCards.length - 1 ? (
+      `
+      <div class="benefit-card-large">
+        <div class="last-card-left">
+          <div class="card-title-container">
+            <img src="${card.icon}" alt="${card.title}" class="card-icon-container">
+            <h3>${card.title}</h3>
+          </div>
+          <p>${card.text}</p>
+        </div>
+        <div class="last-card-right">
+          <div class="card-ornamental-left"></div>
+          <div class="card-ornamental-right"></div>
+        </div>
+      </div>
+    `
+    ) : (
+      `
+      <div class="benefit-card-common">
+        <div class="card-title-container">
+          <img src="${card.icon}" alt="${card.title}" class="card-icon-container">
+          <h3>${card.title}</h3>
+        </div>
+        <p>${card.text}</p>
+      </div>
+    `
+    );
+    if (idx === featuresCards.length - 1) {
+      cardElement.classList.add("last-benefit-card");
+      cardElement.style.backgroundColor = "#F3F2FF";
     }
+
+    cardElement.style.width = (idx === 1 || idx === 2) ? "382px" : idx === featuresCards.length - 1 ? "" : "300px";
     benefitsContainer.appendChild(cardElement);
   });
 });
