e40ec90e9cfd86b40a55aff388f2d521b87a9691
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2025-01-08 10:18:25 -0300

    feat: contact page finished

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-const appFeatures = [
-  {
-    icon: `assets/images/cash-icon.svg`,
-    title: "Cashback",
-    text: "Receba parte do seu dinheiro de volta em todas as suas compras.",
-  },
-  {
-    icon: `assets/images/gift-icon.svg`,
-    title: "Ofertas",
-    text: "Acesse promoções exclusivas diretamente no app.",
-  },
-  {
-    icon: `assets/images/credit-card-icon.svg`,
-    title: "Segurança",
-    text: "Bloqueie e configure seu cartão com facilidade.",
-  },
-]
-
-for (let i = 1; i <= 5; i++) {
-  const imgPath = `assets/avatares/avatar-0${i}.png`;
-  userAvatares.push(imgPath);
-}
-
 const pageName = document.location.pathname;
 
 if (pageName === "/index.html") {
   const menuOptions = document.getElementsByClassName("home");
   menuOptions[0].style.color = "#6729FF";
+} else if (pageName === "/contact.html") {
+  const menuOptions = document.getElementsByClassName("contact");
+  menuOptions[0].style.color = "#6729FF";
 }
+
+const nameInputElement = document.getElementById("name");
+const lastNameInputElement = document.getElementById("lastName");
+const messageInputElement = document.getElementById("message");
+const emailInputElement = document.getElementById("email");
+const buttonElement = document.getElementById("form-button");
+
+buttonElement.addEventListener("click", () => {
+  const name = nameInputElement.value;
+  const lastName = lastNameInputElement.value;
+  const email = emailInputElement.value;
+  const message = messageInputElement.value;
+
+  const isOk = false;
+
+  // Validations
+  if (!name) {
+    nameInputElement.style.border = "1px solid red";
+    const errorMsg = document.createElement("p");
+    errorMsg.classList.add("error-msg");
+    errorMsg.textContent = "Por favor, preencha o campo de nome";
+    nameInputElement.parentNode.appendChild(errorMsg);
+  }
+  if (!lastName) {
+    lastNameInputElement.style.border = "1px solid red";
+  }
+  if (!email) {
+    emailInputElement.style.border = "1px solid red";
+  }
+  if (!message) {
+    messageInputElement.style.border = "1px solid red";
+  }
+  
+  if (isOk) {
+    const formData = {
+      name: name,
+      lastName: lastName,
+      email: email,
+      message: message,
+    };
+  
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }
+});
